b45d5a91668dd7bc91ab1f6c9307b334ff0bbb3a
Anna <anyaamdmitrieva@yandex.ru>
2022-10-22 19:30:59 +0600

    add seminar2 common

---
+++ Common2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int rem1 = number1 / number2;
+int rem2 = number1 % number2;
+
+if ( rem2 != 0)
+{
+    Console.WriteLine($"Не кратно, остаток {rem2}");
+}
+else
+{
+    Console.WriteLine("Кратно");
+}
