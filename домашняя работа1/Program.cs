caa97bbedfcf010a852cb745ff193229f148d093
Anna <anyaamdmitrieva@yandex.ru>
2022-10-22 16:30:47 +0600

    add seminar2 home work1

--- домашняя работа1/Program.cs
+++ домашняя работа1/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Write("Введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 3: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+
+if (max > number2)
+{
+    if (max > number3)
+    {
+        Console.WriteLine($"Максимальное число {max}");
+    }
+}
+
+else
+{
+    max = number2;
+    if (number2 > number3)
+    {
+       Console.WriteLine($"Максимальное число {max}"); 
+    }
+    else
+    {
+        max = number3;
+        Console.WriteLine($"Максимальное число {max}");
+    }
+}
+
+Console.ReadKey();
