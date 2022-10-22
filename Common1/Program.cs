dfa904123bb0389a13ad715ea9e02505825a4589
Anna <anyaamdmitrieva@yandex.ru>
2022-10-22 19:35:18 +0600

    add seminar2 common

---
+++ Common1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+int number = new Random().Next(100, 1000);
+
+Console.WriteLine(number); 
+
+int a1 = number % 10;
+int number2 = ( number - a1 ) / 10;
+int a2 =  number2 % 10;
+int number3 = ( number2 - a2 ) / 10;
+int number4 = number3 * 10 + a1;
+
+Console.WriteLine(number4);
+
