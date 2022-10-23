d7d4951931f0d4657dc5f31d5eaba784eff40583
Anna <anyaamdmitrieva@yandex.ru>
2022-10-23 14:53:57 +0600

    modif common3

--- Common3/Program.cs
+++ Common3/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Console.Write("Введите число 1: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+int number2 = int.Parse(Console.ReadLine());
+
+double result = Math.Pow(number1, 2);
+
+if (result == number2)
+{
+    Console.WriteLine($"Число {number1} квадрат {number2}");
+}
+else Console.WriteLine($"Число {number1} не квадрат {number2}");
