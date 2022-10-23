4e91ef14468c40c612a4b2eeebedc219031aec82
Anna <anyaamdmitrieva@yandex.ru>
2022-10-23 15:05:34 +0600

    modif common4

---
+++ Common4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int rem1 = number % 23;
+int rem2 = number % 7;
+
+if(rem1 == 0 && rem2 == 0)
+{
+    Console.WriteLine($"Число {number} кратно 7 и 23.");
+}
+else Console.WriteLine($"Число {number} не кратно 7 и 23.");
